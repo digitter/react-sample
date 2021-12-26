34994927252d315757d37feb2d82edf921abd58e
tanida.kazuki <tanida.kazuki@moneyforward.co.jp>
2021-12-26 21:42:45 +0900

    add BookList component to show books from api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import BookList from './componets/BookList';
 
 function App() {
   return (
@@ -18,6 +19,10 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <main>
+        <BookList />
+      </main>
     </div>
   );
 }
---
+++ src/componets/BookList.jsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react'
+
+export default function BookList() {
+  const [books, setBooks] = useState([])
+  const [selectedBook, setSelectedBook] = useState({})
+
+  useEffect(() => {
+    fetch('http://localhost:3001/books')
+      .then(response => response.json())
+      .then(books => setBooks(books))
+  }, [])
+
+  const handleShowDetails = book => {
+    setSelectedBook(book)
+  }
+
+  return (
+    <div>
+      <h1>Book List</h1>
+      <div style={{display: 'flex', flexDirection: 'row'}}>
+        <table style={{marginRight: 50}}>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Title</th>
+            </tr>
+          </thead>
+          <tbody>
+            {books.map((book, index) => {
+              return (
+                <tr key={index}>
+                  <td >{book.id}</td>
+                  <td>{book.title}</td>
+                  <td>
+                    <button onClick={() => handleShowDetails(book)}>
+                      View Details 👀
+                    </button>
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+
+        {Object.keys(selectedBook).length ? (
+            <div style={{border: '5px solid gray'}}>
+              <button onClick={() => handleShowDetails({})}>Close ❌</button>
+
+              <p>ID: {selectedBook.id}</p>
+              <p>Title: {selectedBook.id}</p>
+              <p>Body: {selectedBook.id}</p>
+              <p>Created At: {selectedBook.created_at}</p>
+            </div>
+          ) : null
+        }
+      </div>
+    </div>
+  )
+}
