6440bb048638e9f8bb07a515338d0f1fe881897c
tanida.kazuki <tanida.kazuki@moneyforward.co.jp>
2021-12-28 22:50:04 +0900

    show book detals in modal window and modify date time format for readability.

--- src/componets/BookList.jsx
+++ src/componets/BookList.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { DateTime } from 'luxon'
 
 export default function BookList() {
   const [books, setBooks] = useState([])
@@ -10,50 +11,50 @@ export default function BookList() {
       .then(books => setBooks(books))
   }, [])
 
-  const handleShowDetails = book => {
-    setSelectedBook(book)
-  }
+  const handleShowDetails = book => setSelectedBook(book)
 
   return (
-    <div>
+    <>
       <h1>Book List</h1>
-      <div style={{ display: 'flex', flexDirection: 'row' }}>
-        <table style={{ marginRight: 50 }}>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Title</th>
-            </tr>
-          </thead>
-          <tbody>
-            {books.map((book, index) => {
-              return (
-                <tr key={index}>
-                  <td >{book.id}</td>
-                  <td>{book.title}</td>
-                  <td>
-                    <button onClick={() => handleShowDetails(book)}>
-                      View Details 👀
-                    </button>
-                  </td>
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>
 
-        {Object.keys(selectedBook).length ? (
-          <div style={{ border: '5px solid gray' }}>
-            <button onClick={() => handleShowDetails({})}>Close ❌</button>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+          </tr>
+        </thead>
+        <tbody>
+          {books.map((book, index) => {
+            return (
+              <tr key={index}>
+                <td >{book.id}</td>
+                <td>{book.title}</td>
+                <td>
+                  <button onClick={() => handleShowDetails(book)}>
+                    View Details 👀
+                  </button>
+                </td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
 
-            <p>ID: {selectedBook.id}</p>
-            <p>Title: {selectedBook.title}</p>
-            <p>Body: {selectedBook.body}</p>
-            <p>Created At: {selectedBook.created_at}</p>
+      {Object.keys(selectedBook).length ? (
+          <div id="overlay">
+            <div id="book-details">
+              <button onClick={() => handleShowDetails({})}>
+                Close ✖️
+              </button>
+              <p>ID: {selectedBook.id}</p>
+              <p>Title: {selectedBook.title}</p>
+              <p>Body: {selectedBook.body}</p>
+              <p>Created At: {DateTime.fromISO(selectedBook.created_at, { zone: 'Asia/Tokyo' }).toLocaleString(DateTime.DATE_MED)}</p>
+            </div>
           </div>
         ) : null
-        }
-      </div>
-    </div>
+      }
+    </>
   )
 }
